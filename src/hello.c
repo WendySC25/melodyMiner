8b049d5d1e9d5dacd7076e2b024f6763c965b880
WendySC <wendy.c2502@gmail.com>
2024-09-24 13:11:25 -0600

    remove - hello.c was removed, it was unnecessary

--- src/hello.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include "hello.h"
-
-void print_hello(void) {
-    char username[] = "DnaFoxdy";
-    printf("Hello World, %s!\n", username); 
-    printf("You do not have a main, %s!\n", username); 
-}
--- src/hello.h
+++
@@ -1,6 +0,0 @@
-#ifndef PRINT_H
-#define PRINT_H
-
-void print_hello(void);
-
-#endif // PRINT_H
