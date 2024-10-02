933376c3bc716faafa3c4ef9c1fd17499e68a87f
WendySC <wendy.c2502@gmail.com>
2024-10-02 14:42:20 -0600

    refactor - All the headers of the music database are concentrated in one

---
+++ include/database/DatabaseUtils.h
@@ -0,0 +1,16 @@
+#ifndef DATABASEUTILS_H
+#define DATABASEUTILS_H
+
+#include "include/Database.h"
+
+#include "include/database/models/Album.h"
+#include "include/database/models/Group.h"
+#include "include/database/models/Performer.h"
+#include "include/database/models/Person.h"
+
+#include "include/database/daos/BaseDAO.h"
+#include "include/database/daos/AlbumDAO.h"
+#include "include/database/daos/PerformerDAO.h"  
+
+
+#endif 
