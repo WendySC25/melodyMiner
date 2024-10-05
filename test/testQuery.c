c3e75e7a63ce2518fcb88436d79caf1ab3b5bc13
WendySC <wendy.c2502@gmail.com>
2024-10-05 17:14:47 -0600

    test - The environment was configured to perform test-driven development to implement the language that generates SQL queries

---
+++ test/testQuery.c
@@ -0,0 +1,19 @@
+#include "third_party/unity/src/unity.h"
+
+void setUp(void) {
+   
+}
+
+void tearDown(void) {
+    
+}
+
+void test_suma(void) {
+    
+}
+
+int main(void) {
+    UNITY_BEGIN();
+    RUN_TEST(test_suma);
+    return UNITY_END();
+}
