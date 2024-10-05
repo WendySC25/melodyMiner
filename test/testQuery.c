703651c7e91887bd47bb24dfb899da71b1590418
WendySC <wendy.c2502@gmail.com>
2024-10-05 17:33:26 -0600

    test - Test for the search by song title query

--- test/testQuery.c
+++ test/testQuery.c
@@ -8,12 +8,26 @@ void tearDown(void) {
     
 }
 
-void test_suma(void) {
-    
+void testQueryTitle(void) {
+    const char* input = "title[love]"; 
+    const char* expected = "SELECT * FROM rolas r WHERE (r.title LIKE '%love%')";
+    const char* resultado = processQuery(input);
+    TEST_ASSERT_EQUAL_STRING(expected, resultado);
+
+    input = "title[love|happy]";
+    expected = "SELECT * FROM rolas r WHERE (r.title LIKE '%love%' OR r.title LIKE '%happy%')";
+    const char* resultado = processQuery(input);
+    TEST_ASSERT_EQUAL_STRING(expected, resultado);
+
+    input = "title[love&happy]";
+    expected = "SELECT * FROM rolas r WHERE (r.title LIKE '%love%' AND r.title LIKE '%happy%')";
+    const char* resultado = processQuery(input);
+    TEST_ASSERT_EQUAL_STRING(expected, resultado);
+
 }
 
 int main(void) {
     UNITY_BEGIN();
-    RUN_TEST(test_suma);
+    RUN_TEST(testQueryTitle);
     return UNITY_END();
 }
