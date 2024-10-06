94becea532c0a092557da96d608b37542ac55330
WendySC <wendy.c2502@gmail.com>
2024-10-05 18:31:04 -0600

    add dependency - Google Test added for unit testing

--- src/query/queryProcessor.h
+++ src/query/queryProcessor.h
@@ -8,6 +8,8 @@
 #include <sstream>
 
 std::string join(const std::vector<std::string>& vec, const std::string& delim);
+std::regex generateRegex(const std::string& field);
+std::string getCondition(const std::string& field, const std::string& query, const std::string& con);
 std::string processQuery(const std::string& query);
 
 
