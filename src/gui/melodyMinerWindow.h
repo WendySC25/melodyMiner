feed13902007810a32abb2921eddfb8ec826ad8d
WendySC <wendy.c2502@gmail.com>
2024-10-10 20:44:26 -0600

    gui - main window of the interface with its respective controller. It already shows the songs from the database

---
+++ src/gui/melodyMinerWindow.h
@@ -0,0 +1,88 @@
+#ifndef GTKMM_MELODYMINERWINDOW_H
+#define GTKMM_MELODYMINERWINDOW_H
+
+#include <gtkmm.h>
+#include <gdkmm.h>
+#include <gtkmm/application.h>
+#include <gtkmm/cssprovider.h>
+#include <gtkmm/stylecontext.h>
+#include <gtkmm/filechooserdialog.h>
+#include <gio/gio.h> 
+
+#include <memory>
+#include <array>
+#include <iostream>
+
+#include "src/gui/RolaDetailview.h"
+#include "src/gui/dialog/FormatEditRola.h"
+#include "src/gui/dialog/RolaController.h"
+#include "src/query/queryProcessor.h"
+#include "src/gui/MinerDialog.h"
+#include "include/database/DatabaseUtils.h"
+
+
+
+class MelodyMinerWindow : public Gtk::Window{
+    public:
+        MelodyMinerWindow();
+        ~MelodyMinerWindow() override;
+        Gtk::Window* get_window();
+
+    protected:
+        class ModelColumns;
+        Glib::RefPtr<Gtk::Builder> builder;
+
+        void on_button_quit();
+        void on_item_activated(unsigned int position);
+        void on_selection_changed();
+        void on_setup_listitem(const Glib::RefPtr<Gtk::ListItem>& list_item);
+        void on_bind_listitem(const Glib::RefPtr<Gtk::ListItem>& list_item);
+        int on_model_sort(const Glib::RefPtr<const ModelColumns>& a, const Glib::RefPtr<const ModelColumns>& b);
+        void fillModelFromRolas(const std::vector<Rola>& rolas);
+        void add_entry(const std::string& filename, const Glib::ustring& description, const Rola& rola);
+
+        void on_search_entry_changed();
+        void on_search_button_clicked();
+        void updateModelFromRolas(const std::vector<Rola>& rolas);
+        void clearModel();
+
+        void on_select_file_button_clicked();
+        void on_folder_dialog_finish(const Glib::RefPtr<Gio::AsyncResult>& result, const Glib::RefPtr<Gtk::FileDialog>& dialog);
+
+
+        Glib::RefPtr<Gio::ListStore<ModelColumns>> m_data_model;
+        Glib::RefPtr<Gtk::SingleSelection> m_selection_model;
+        Glib::RefPtr<Gtk::SignalListItemFactory> m_factory;
+
+        Gtk::Window* m_MainWindow;
+        Gtk::ScrolledWindow* m_ScrolledWindow;
+        Gtk::GridView* m_GridView;
+        Gtk::Box* m_ButtonBox;
+        Gtk::Button* m_Button_Quit;
+        Gtk::Entry* m_SearchEntry;
+        Gtk::Button* m_SearchButton;
+        Gtk::Button* m_SelectFileButton;
+
+        class ModelColumns : public Glib::Object{
+            public:
+                std::string m_filename;
+                Glib::ustring m_description;
+                Glib::RefPtr<Gdk::Texture> m_texture;
+
+                Rola m_rola;
+
+                static Glib::RefPtr<ModelColumns> create(const std::string& filename,
+                    const Glib::ustring& description, const Glib::RefPtr<Gdk::Texture>& texture, const Rola& rola){
+                    return Glib::make_refptr_for_instance<ModelColumns>(new ModelColumns(filename, description, texture, rola));
+                }
+
+            protected:
+                ModelColumns(const std::string& filename, const Glib::ustring& description,
+                    const Glib::RefPtr<Gdk::Texture>& texture, const Rola& rola)
+                    : m_filename(filename), m_description(description), m_texture(texture), m_rola(rola)
+                {}
+        }; 
+};
+        
+
+#endif // GTKMM_MELODYMINERWINDOW_H
